9ab7786f329c34de5936b3809057f62eb5c1b22d
DonkeySlonk <166477199+DonkeySlonk@users.noreply.github.com>
2024-05-29 19:24:54 +1000

    Intro Scene Dump

---
+++ Assets/Scripts/LoadSceneOnKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnKey : MonoBehaviour 
+{
+	public KeyCode key;
+	[Tooltip("Exact name of the scene to load upon the button press")]
+	public string sceneName;
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyUp (key)) 
+		{
+			SceneManager.LoadScene (sceneName);
+		}
+	}
+}
